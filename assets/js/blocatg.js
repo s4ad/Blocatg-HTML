bee7bea9f41fa44c38d3b5ef026b1b285eb7236f
Abderrahmane Naceur <nacer15oct@gmail.com>
2016-12-14 10:22:10 +0100

    add content and sidebar section

---
+++ assets/js/blocatg.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    //rtl:true,
+    loop:true,
+    margin:0,
+    nav:true,
+    autoPlay: 10,
+    transitionStyle: 'fadeUp',
+    navText :['<span><i class="fa fa-angle-left"></i></span>',
+                '<span><i class="fa fa-angle-right"></i></span>'
+                ],
+    items:1,
+});
